55f574854dadb7636a5552f4009cea140e949b0c
Aravindh6122001 <Aravindh.Krishna@bigthinkcode.com>
2023-04-13 15:28:10 +0530

    added access tokens

--- app.py
+++ app.py
@@ -2,51 +2,33 @@ from flask import Flask, request, session, redirect, url_for, render_template, f
 import psycopg2 # psycopg2 
 import psycopg2.extras
 from functools import wraps
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, create_refresh_token, get_jwt_identity
+import os
+from dotenv import load_dotenv
 import re 
 import jwt
 import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
- 
+
+load_dotenv() 
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+
 app = Flask(__name__)
-app.secret_key = 'cairo-ednalan'
- 
-DB_HOST = "localhost"
-DB_NAME = "postgres"
-DB_USER = "postgres"
-DB_PASS = "Aravindh@01"
+app.secret_key = os.getenv("key")
+# JWT_SECRET_KEY = 'JWT_SECRET_KEY'
+JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
+JWTManager(app)
  
 conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
 cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 
-def validate_access_token(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        access_token = request.headers.get('Authorization')
-
-        if not access_token:
-            return jsonify({'error': 'Access token is missing'}), 401
-        try:
-            data = jwt.decode(access_token,app.config["secret_key"]) 
-        except:
-            return jsonify({'error': 'Invalid Access token'}), 401
-
-        return f(*args, **kwargs)
-            
-    return decorated_function
-
-     #   auth = request.authorization
-     #      if auth and auth.password == 'password':
-     #       access_token = jwt.encode({'user': auth.username,'exp':datetime.datetime.utcnow() + datetime.timedelta      (second=30)})
-          
-     #      return jsonify({'token' : access_token.decode('UTF-8')})
-     
-     #    return jsonify("could not verify")
-
-
-@app.route('/register',methods=['GET','POST'])
+@app.route('/register',methods=['POST'])
 def register():
-     if request.method == 'POST':
+    #  if request.method == 'POST':
        required_data = request.get_json()
        if not required_data:
             return jsonify(message="No JSON data provided"), 400
@@ -73,7 +55,6 @@ def register():
 
 
 @app.route('/login',methods=['GET','POST'])
-@validate_access_token
 def login():
     if request.method == 'POST':
        required_data = request.get_json()
@@ -91,24 +72,34 @@ def login():
           'email' : required_data['email']
        }       
             
-       cursor.execute('SELECT password FROM users WHERE (username,email) = (%s,%s)', (new_data['username'],new_data['email'],))
+       cursor.execute('SELECT * FROM users WHERE (username,email) = (%s,%s)', (new_data['username'],new_data['email'],))
        account = cursor.fetchone()
        
 
        if account is None:
             return jsonify(message="Login Failed,please try again"), 401
        password_rs = account['password']
- 
+       
        if check_password_hash(password_rs,new_data['password']):
-            return jsonify(message = "Login successful"),200
-       else:
-            return jsonify(message="Wrong Password"),500
-   
-    elif request.method == 'GET':
-         return jsonify("This is get method")      
+                  refresh = create_refresh_token(identity = account['password'])
+                  access = create_access_token(identity = account['password'])
+                  return ({
+                        'user':{
+                            'refresh' : refresh,
+                            'access' : access,
+                            'username' : account['username'],
+                            'email' : account['email']
+                        },
+                        "success":"Login successful"
+                    })
+    return ({
+            "error":"Login Unsuccessful. Please check username and password"
+        })  
+     
+       
 
 
-@app.route('/login/<string:name>',methods=['GET','POST'])
+@app.route('/login/<string:name>',methods=['GET'])
 def login_item(name):
  if request.method == 'GET':
 
@@ -117,15 +108,41 @@ def login_item(name):
    stores = cursor.fetchall()
    for store in stores:
       if (store[0] == name):
-        return jsonify(store[1] , "message created successfully!!")
+        return jsonify(store[1] ,"message created successfully!!")
     
    return jsonify("message : not found")
 
+# User.query.filter_by(id=current_user_id).first()
 
+@app.route('/protected', methods=['GET'])
 
-@app.route("/delete", methods=["DELETE"])
-def delete():
+    
+
+@app.route("/update_details", methods=["PATCH","DELETE","GET"])
+@jwt_required()
+def update():
     required_data = request.get_json()
+    
+    if request.method == "PATCH":
+        new_data = {
+            'username': required_data['username'],
+            'email':required_data['email']
+            }
+        
+        cursor.execute('SELECT * FROM users WHERE (username,email) = (%s,%s)', (new_data['username'],new_data['email'],))
+        account = cursor.fetchall()
+        if account is None:
+            return jsonify(message="Invalid details")
+        
+        try:
+            cursor.execute("UPDATE users SET username=%s WHERE email=%s",(new_data['username'], new_data['email']))
+            conn.commit()
+            # return jsonify({'message': f'Protected endpoint. Welcome, {account['username']}!'}), 200
+            return jsonify(message="Updated")
+       
+        except:
+            return jsonify(message= "Patch error")
+      
     
     if request.method == "DELETE":
         new_data = {
@@ -138,24 +155,7 @@ def delete():
         return jsonify(message="content deleted successful")
    
     except:
-         return jsonify(message= "there was a problem deleting that row")
-    
-
-
-@app.route("/patch", methods=["PATCH"])
-def update():
-    required_data = request.get_json()
-    if request.method == "PATCH":
-        new_data = {
-            'username': required_data['username'],
-            'email':required_data['email']}
-        try:
-            account = cursor.execute("UPDATE users SET username=%s WHERE email=%s", (new_data['username'], new_data['email']))
-            conn.commit()
-            return jsonify(message="Updated")
-       
-        except:
-            return jsonify(message= "Patch error")
+         return jsonify(message= "there was a problem deleting that row")    
        
     
 if __name__ == "__main__":
